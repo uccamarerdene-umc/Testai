e915bd9e28337191d5049a6cebdb7049d76992c4
uccamarerdene-umc <uccamarerdene@gmail.com>
2026-03-26 11:56:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ if not google_api_key or not pinecone_api_key:
 @st.cache_resource
 def load_models():
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-002",
+        model="models/embedding-001",
         google_api_key=google_api_key
     )
     return embeddings
